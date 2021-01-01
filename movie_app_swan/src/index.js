78455be3895b0025a3b5b939916b682bcd802fe5
csh94 <speed9116@naver.com>
2021-01-01 10:23:55 +0900

    Learn about JSX and PropTypes

--- movie_app_swan/src/index.js
+++ movie_app_swan/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import Potato from "./Potato";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />, document.getElementById('potato')
 );
+/* #2.0
+<App /> 은 Component, HTML를 반환하는 함수.
+react는 component를 사용해서 HTML처럼 작성하려는 경우 필요
+이러한 javascript와 HTML 사이의 조합을 jsx라 부름.
+jsx는 오직 react에서만 사용하는 것임.딴데서 사용 x
+즉 jsx는 javascript안의 HTML이야. Component를 만들고 어떻게 사용하는지에 대한 것
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+그리고 기억해야할 것은 react application은 
+한번에 하나의 component만 rendering 할수 있다는 것.
+그래서 Potato를 여기서 import 하는 것이 아닌 App안에서 하는것.
+여러 컴포넌트를 App내부에 쓰는거야 잊지마!
+*/
